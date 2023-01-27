dbd16337b7768480975823224b0473bcef9c325e
jjurewicz <123731741+jjurewicz@users.noreply.github.com>
2023-01-27 14:45:35 +0100

    Add files via upload

---
+++ remind_me.R
@@ -0,0 +1,21 @@
+# Assignment 3.2R
+# Author Jagoda Jurewicz 
+
+#Q3.2R.2
+
+# REMIND ME FUNCTION
+
+remind_me <- function(){
+  important_dates <- list(
+    "Last Team Meeting - 31/01/2023",
+    "Programming Exam - 03/02/2023",
+    "Internship Start - 06/02/2023",
+    "Valentine's Day - 14/02/2023",
+    "Mom's Bday - 10/03/2023",
+    "Parents' Wedding Anniversary - 17/04/2023",
+    "My Bday Party - 18/05/2022"
+  )
+  return(important_dates)
+}
+
+remind_me()
